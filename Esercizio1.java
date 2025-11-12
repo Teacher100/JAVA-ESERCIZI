db9506b838e3ff81d48cbe4d712adfd622ceb967
teacher100 <aurora.calandra@marconigorgonzola.itgit config user.email aurora.calandra@marconigorgonzola.it>
2025-11-12 19:52:20 +0100

    primo saluto

--- Esercizio1.java
+++ Esercizio1.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
 class Esercizio1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("ciao" );
     }
 }
